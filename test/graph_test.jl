73a217a8504e91f68ced375d52c6773d0ec2ae08
Parker J. Rule <parker.rule@tufts.edu>
2021-07-15 09:14:40 -0400

    Add scaffolding for test suite

---
+++ test/graph_test.jl
@@ -0,0 +1,14 @@
+include("../test.jl")
+using GerryChain
+using Test
+
+@testset "Loading a graph" begin
+    @testset "2x2 graph" begin
+        graph = load_graph_from_edge_list("data/graph.dat", "data/weight.dat")
+        @test graph.num_nodes == 4
+        @test graph.num_edges == 4
+        @test graph.populations == [1, 2, 4, 8]
+        @test graph.total_pop == 15
+        # TODO: add more assertions here if you feel like it :)
+    end
+end
