d4f87fdde4ce5b9d4829a8331aa656423ea33711
Mai <nguyenm2151@gmail.com>
2021-07-22 17:13:43 +0700

    Add test cases

--- test/graph_test.jl
+++ test/graph_test.jl
@@ -3,12 +3,14 @@ using GerryChain
 using Test
 
 @testset "Loading a graph" begin
-    @testset "2x2 grid" begin
-        graph = load_graph_from_edge_list("data/2x2_grid.dat", "data/2x2_grid_weight.dat")
+    @testset "2x2 graph" begin
+        graph = load_graph_from_edge_list("data/2x2_graph.dat", "data/2x2_graph_weight.dat")
         @test graph.num_nodes == 4
         @test graph.num_edges == 4
         @test graph.populations == [1, 2, 4, 8]
         @test graph.total_pop == 15
+        @test graph.adj_matrix == [0 1 2 0; 1 0 3 4; 2 3 0 0; 0 4 0 0]
+        @test graph.neighbors == [[2, 3], [1, 3, 4], [1, 2], [2]]
     end
     @testset "2x2 triangular lattice" begin 
         graph = load_graph_from_edge_list("data/2x2_triangular_lattice.dat", "data/2x2_triangular_lattice_weight.dat")
@@ -16,5 +18,6 @@ using Test
         @test graph.num_nodes == 6
         @test graph.populations == [1, 2, 4, 8, 10, 12]
         @test graph.total_pop == 37
+        @test graph.neighbors == [[2, 3], [1, 3, 4], [1, 2, 4, 5, 6], [2, 3, 6], [3,6], [3, 4, 5]]
     end 
 end
--- test/partition_test.jl
+++ test/partition_test.jl
@@ -3,13 +3,20 @@ using GerryChain
 using Test
 
 
-
 @testset "Loading a partition" begin
+    graph = load_graph_from_edge_list("data/2x2_graph.dat", "data/2x2_graph_weight.dat")
     @testset "0 1 1 1" begin
-        graph = load_graph_from_edge_list("data/2x2_grid.dat", "data/2x2_weight.dat")
         partition = load_partition_from_line(graph,"0 1 1 1")
-        @test partition.num_districts = 2 
-        @test partition.assignments = [1, 2, 2, 2]
-        @test partition.dist_populations [1, 14]
+        @test partition.num_dists == 2 
+        @test partition.num_cut_edges == 2 
+        @test partition.assignments == [1, 2, 2, 2]
+        @test partition.dist_populations == [1, 14]
+    end
+    @testset "0 2 1 1" begin
+        partition = load_partition_from_line(graph,"0 2 1 1")
+        @test partition.num_dists == 3 
+        @test partition.num_cut_edges == 4
+        @test partition.assignments == [1, 3, 2, 2]
+        @test partition.dist_populations == [1, 12, 2]
     end
 end
