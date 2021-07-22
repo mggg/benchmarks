52887caed7d4d9d45ae6f46f349d39a2c67ae44f
Mai <nguyenm2151@gmail.com>
2021-07-22 15:36:24 +0700

    Add more test cases and data

--- test/graph_test.jl
+++ test/graph_test.jl
@@ -3,11 +3,18 @@ using GerryChain
 using Test
 
 @testset "Loading a graph" begin
-    @testset "2x2 graph" begin
-        graph = load_graph_from_edge_list("data/graph.dat", "data/weight.dat")
+    @testset "2x2 grid" begin
+        graph = load_graph_from_edge_list("data/2x2_grid.dat", "data/2x2_grid_weight.dat")
         @test graph.num_nodes == 4
         @test graph.num_edges == 4
         @test graph.populations == [1, 2, 4, 8]
         @test graph.total_pop == 15
     end
+    @testset "2x2 triangular lattice" begin 
+        graph = load_graph_from_edge_list("data/2x2_triangular_lattice.dat", "data/2x2_triangular_lattice_weight.dat")
+        @test graph.num_edges == 9
+        @test graph.num_nodes == 6
+        @test graph.populations == [1, 2, 4, 8, 10, 12]
+        @test graph.total_pop == 37
+    end 
 end
--- test/partition_test.jl
+++ test/partition_test.jl
@@ -2,11 +2,12 @@ include("../test.jl")
 using GerryChain
 using Test
 
-graph = load_graph_from_edge_list("data/graph.dat", "data/weight.dat")
-partition = load_partition_from_line(graph,"0 1 1 1")
+
 
 @testset "Loading a partition" begin
     @testset "0 1 1 1" begin
+        graph = load_graph_from_edge_list("data/2x2_grid.dat", "data/2x2_weight.dat")
+        partition = load_partition_from_line(graph,"0 1 1 1")
         @test partition.num_districts = 2 
         @test partition.assignments = [1, 2, 2, 2]
         @test partition.dist_populations [1, 14]
